c474bdde9426bf570f6fd758aa8034fdb282a531
stephenzwj <stephenzwj.gmail.com>
2022-09-25 14:17:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ echo "Building $1 and generating pdf"
 # lyx --export pdf4 $1
 # split the the $1 string according to the comma
 echo $1 | sed -n 1'p' | tr ',' '\n' | while read word; do
-    lyx --export pdf4 $word
+    echo "Generating pdf for $word"
+    lyx --export pdf4 $word || true
     echo "Generated pdf for $word"
 done
-
-        
